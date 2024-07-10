aad21a349bf0c35e9d1952eb3787a4bf81f49985
Houssam ZOUARI <66483200+HoussamZF@users.noreply.github.com>
2024-07-10 23:06:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import pickle
 import numpy as np
+import xgboost as xgb
 
 app = FastAPI()
 
-# Load the trained model
-model = pickle.load(open('model.pkl', 'rb'))
+# Get absolute path to model file
+model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+
+# Load the XGBoost model using xgb.Booster
+model = xgb.Booster(model_file=model_path)
 
 # Set up templates and static files
 templates = Jinja2Templates(directory="templates")
@@ -30,17 +34,18 @@ async def predict(request: Request):
     Patient_age = data.get('Patient_age')
 
     # Prepare the input data
-    input_data = np.array([[Plasma_glucose, Blood_Work_R1, Blood_Pressure, Blood_Work_R3, BMI, Blood_Work_R4, Patient_age]])
-    input_data = input_data.astype(np.float64)
-    
+    input_data = np.array([[Plasma_glucose, Blood_Work_R1, Blood_Pressure, Blood_Work_R3, BMI, Blood_Work_R4, Patient_age$
+    input_data = input_data.astype(np.float32)  # Adjust dtype if necessary
+
     # Make prediction
-    prediction = model.predict(input_data)
-    
+    dmatrix = xgb.DMatrix(input_data)
+    prediction = model.predict(dmatrix)
+
     # Convert numpy data types to standard Python data types
     prediction = prediction[0].item()
-    
+
     return {"prediction": prediction}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
